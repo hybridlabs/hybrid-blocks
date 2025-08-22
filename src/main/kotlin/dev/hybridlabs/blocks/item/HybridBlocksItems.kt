87ab847328b99e0e4b910b6667375ffc89b85127
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-22 11:49:37 +0300

    Started on Pinwheel and Basketweave bricks

--- src/main/kotlin/dev/hybridlabs/blocks/item/HybridBlocksItems.kt
+++ src/main/kotlin/dev/hybridlabs/blocks/item/HybridBlocksItems.kt
@@ -64,6 +64,40 @@ object HybridBlocksItems {
     val RED_STAINED_BRICKS = register("red_stained_bricks") { BlockItem(HybridBlocksBlocks.RED_STAINED_BRICKS, it) }
     val BLACK_STAINED_BRICKS = register("black_stained_bricks") { BlockItem(HybridBlocksBlocks.BLACK_STAINED_BRICKS, it) }
 
+    val WHITE_STAINED_PINWHEEL_BRICKS = register("white_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.WHITE_STAINED_PINWHEEL_BRICKS, it) }
+    val ORANGE_STAINED_PINWHEEL_BRICKS = register("orange_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.ORANGE_STAINED_PINWHEEL_BRICKS, it) }
+    val MAGENTA_STAINED_PINWHEEL_BRICKS = register("magenta_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.MAGENTA_STAINED_PINWHEEL_BRICKS, it) }
+    val LIGHT_BLUE_STAINED_PINWHEEL_BRICKS = register("light_blue_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.LIGHT_BLUE_STAINED_PINWHEEL_BRICKS, it) }
+    val YELLOW_STAINED_PINWHEEL_BRICKS = register("yellow_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.YELLOW_STAINED_PINWHEEL_BRICKS, it) }
+    val LIME_STAINED_PINWHEEL_BRICKS = register("lime_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.LIME_STAINED_PINWHEEL_BRICKS, it) }
+    val PINK_STAINED_PINWHEEL_BRICKS = register("pink_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.PINK_STAINED_PINWHEEL_BRICKS, it) }
+    val GRAY_STAINED_PINWHEEL_BRICKS = register("gray_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.GRAY_STAINED_PINWHEEL_BRICKS, it) }
+    val LIGHT_GRAY_STAINED_PINWHEEL_BRICKS = register("light_gray_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.LIGHT_GRAY_STAINED_PINWHEEL_BRICKS, it) }
+    val CYAN_STAINED_PINWHEEL_BRICKS = register("cyan_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.CYAN_STAINED_PINWHEEL_BRICKS, it) }
+    val PURPLE_STAINED_PINWHEEL_BRICKS = register("purple_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.PURPLE_STAINED_PINWHEEL_BRICKS, it) }
+    val BLUE_STAINED_PINWHEEL_BRICKS = register("blue_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.BLUE_STAINED_PINWHEEL_BRICKS, it) }
+    val BROWN_STAINED_PINWHEEL_BRICKS = register("brown_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.BROWN_STAINED_PINWHEEL_BRICKS, it) }
+    val GREEN_STAINED_PINWHEEL_BRICKS = register("green_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.GREEN_STAINED_PINWHEEL_BRICKS, it) }
+    val RED_STAINED_PINWHEEL_BRICKS = register("red_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.RED_STAINED_PINWHEEL_BRICKS, it) }
+    val BLACK_STAINED_PINWHEEL_BRICKS = register("black_stained_pinwheel_bricks") { BlockItem(HybridBlocksBlocks.BLACK_STAINED_PINWHEEL_BRICKS, it) }
+
+    val WHITE_STAINED_BASKETWEAVE_BRICKS = register("white_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.WHITE_STAINED_BASKETWEAVE_BRICKS, it) }
+    val ORANGE_STAINED_BASKETWEAVE_BRICKS = register("orange_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.ORANGE_STAINED_BASKETWEAVE_BRICKS, it) }
+    val MAGENTA_STAINED_BASKETWEAVE_BRICKS = register("magenta_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.MAGENTA_STAINED_BASKETWEAVE_BRICKS, it) }
+    val LIGHT_BLUE_STAINED_BASKETWEAVE_BRICKS = register("light_blue_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.LIGHT_BLUE_STAINED_BASKETWEAVE_BRICKS, it) }
+    val YELLOW_STAINED_BASKETWEAVE_BRICKS = register("yellow_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.YELLOW_STAINED_BASKETWEAVE_BRICKS, it) }
+    val LIME_STAINED_BASKETWEAVE_BRICKS = register("lime_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.LIME_STAINED_BASKETWEAVE_BRICKS, it) }
+    val PINK_STAINED_BASKETWEAVE_BRICKS = register("pink_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.PINK_STAINED_BASKETWEAVE_BRICKS, it) }
+    val GRAY_STAINED_BASKETWEAVE_BRICKS = register("gray_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.GRAY_STAINED_BASKETWEAVE_BRICKS, it) }
+    val LIGHT_GRAY_STAINED_BASKETWEAVE_BRICKS = register("light_gray_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.LIGHT_GRAY_STAINED_BASKETWEAVE_BRICKS, it) }
+    val CYAN_STAINED_BASKETWEAVE_BRICKS = register("cyan_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.CYAN_STAINED_BASKETWEAVE_BRICKS, it) }
+    val PURPLE_STAINED_BASKETWEAVE_BRICKS = register("purple_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.PURPLE_STAINED_BASKETWEAVE_BRICKS, it) }
+    val BLUE_STAINED_BASKETWEAVE_BRICKS = register("blue_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.BLUE_STAINED_BASKETWEAVE_BRICKS, it) }
+    val BROWN_STAINED_BASKETWEAVE_BRICKS = register("brown_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.BROWN_STAINED_BASKETWEAVE_BRICKS, it) }
+    val GREEN_STAINED_BASKETWEAVE_BRICKS = register("green_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.GREEN_STAINED_BASKETWEAVE_BRICKS, it) }
+    val RED_STAINED_BASKETWEAVE_BRICKS = register("red_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.RED_STAINED_BASKETWEAVE_BRICKS, it) }
+    val BLACK_STAINED_BASKETWEAVE_BRICKS = register("black_stained_basketweave_bricks") { BlockItem(HybridBlocksBlocks.BLACK_STAINED_BASKETWEAVE_BRICKS, it) }
+
     val CRACKED_BRICKS = register("cracked_bricks") { BlockItem(HybridBlocksBlocks.CRACKED_BRICKS, it) }
     val WHITE_STAINED_CRACKED_BRICKS = register("white_stained_cracked_bricks") { BlockItem(HybridBlocksBlocks.WHITE_STAINED_CRACKED_BRICKS, it) }
     val ORANGE_STAINED_CRACKED_BRICKS = register("orange_stained_cracked_bricks") { BlockItem(HybridBlocksBlocks.ORANGE_STAINED_CRACKED_BRICKS, it) }
