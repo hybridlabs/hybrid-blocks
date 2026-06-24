095252e4b5e91476807a730a1e4b0a28905e8c1f
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-24 22:18:09 +0300

    FabricClientPlatformHelper

--- fabric/src/main/java/dev/hybridlabs/blocks/platform/services/FabricClientPlatformHelper.java
+++ fabric/src/main/java/dev/hybridlabs/blocks/platform/services/FabricClientPlatformHelper.java
@@ -1,3 +1,24 @@
 package dev.hybridlabs.blocks.platform.services;
 
-public class FabricClientPlatformHelper implements ClientPlatformHelper {}
+import dev.hybridlabs.blocks.platform.registration.RegistryObject;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Arrays;
+
+public class FabricClientPlatformHelper implements ClientPlatformHelper {
+
+    @Override
+    public <E extends Entity> void registerEntityRenderer(RegistryObject<EntityType<E>> entityType, EntityRendererProvider<E> entityRendererFactory) {}
+
+    @Override
+    public void registerBlockRenderers(RenderType renderType, Block... blocks) {
+        BlockRenderLayerMap registry = BlockRenderLayerMap.INSTANCE;
+        Arrays.stream(blocks).forEach(block -> registry.putBlocks(renderType, blocks));
+    }
+}
