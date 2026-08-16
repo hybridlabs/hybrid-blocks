97df8f92045d549ee4c80ab515f2b93fd0633556
Daniel Orr <orrdaniel73@gmail.com>
2026-08-16 11:14:14 +0100

    chore: kotlin 2.4.10 (kff 5.12.0, flk 1.13.13)

--- neoforge/src/main/java/dev/hybridlabs/blocks/platform/services/ForgePlatformHelper.java
+++ neoforge/src/main/java/dev/hybridlabs/blocks/platform/services/ForgePlatformHelper.java
@@ -5,10 +5,8 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModList;
-import net.neoforged.fml.javafmlmod.FMLModContainer;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
-import thedarkcolour.kotlinforforge.neoforge.KotlinModContainer;
 
 import java.nio.file.Path;
 
@@ -16,13 +14,7 @@ public class ForgePlatformHelper implements PlatformHelper {
 
     public static IEventBus getEventBus() {
         final ModContainer cont = ModList.get().getModContainerById(Constants.MOD_ID).orElseThrow();
-        if (cont instanceof FMLModContainer fmlModContainer) {
-            return fmlModContainer.getEventBus();
-        } else if (cont instanceof KotlinModContainer kotlinModContainer) {
-            return kotlinModContainer.getEventBus$kotlinforforge_langNeoForge();
-        } else {
-            throw new ClassCastException("The container of the mod " + Constants.MOD_ID + " is not a FML one!");
-        }
+        return cont.getEventBus();
     }
 
     @Override
